90eb7ffd9975daf48aa7950e1e4037e6707a8696
cristianfodor <cristii.fodor@gmail.com>
2023-01-28 14:20:04 +0200

    add env

--- src/config.tsx
+++ src/config.tsx
@@ -4,7 +4,7 @@ export const contractAddress =
 export const dAppName = 'example Dapp';
 
 // Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
-export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
+export const walletConnectV2ProjectId = 'da931dce7ad365cbb25b667865d997e5';
 
 export const apiTimeout = 6000;
 export const transactionSize = 15;
